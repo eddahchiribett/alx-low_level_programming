d1ddd9ef845dd07dddf0f4c2b95661d42ec37b87
eddahchiribett <chiribett5@gmail.com>
2022-04-12 12:53:37 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - starting point
+ *
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	 int i;
+	  char name[] = "_putchar";
+	  for (i = 0; i < 8; i++)
+
+		   _putchar(name[i]);
+	   _putchar('\n');
+	   return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - secondary function
+ *
+ * Description: prints each letter of the alphabet, ending with a newline
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
