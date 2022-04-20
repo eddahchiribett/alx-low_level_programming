51f4820533a2d120c11e5b6a792da7dbd0bccd16
eddahchiribett <chiribett5@gmail.com>
2022-04-20 13:22:10 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,28 @@
+#ifndef ALX
+#define ALX
+
+/**
+ *File:alx
+ *Author: Bostone
+ *Description: This file is the main header file and it contains all the decrations of all the functions in the program
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+#endif
